ddd080ca91763bc3d692e03d572aed33c028a7b0
Sushir1997 <sushirsouravnanda@gmail.com>
2022-04-07 10:32:15 +0530

    first commit

---
+++ src/main/java/in/ashokit/PasswordService.java
@@ -0,0 +1,22 @@
+package in.ashokit;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	
+	public static   String encode(String str) {
+		           Encoder encoder= Base64.getEncoder();
+		           
+		return encoder.encodeToString(str.getBytes());
+		
+	}
+	
+	public static String decode(String str) {
+		Decoder decoder = Base64.getDecoder();
+		       byte[] decode=decoder.decode(str);
+		return new String(decode);
+	}
+
+}
